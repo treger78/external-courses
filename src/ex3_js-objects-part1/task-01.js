2d4dd3ccb4b3fd37a101289d1b414fb52791b51e
treger78 <ilya.pustograev.i@mail.ru>
2021-10-04 13:10:18 +0300

    fix: fix ex1 function to arrow decloration

--- src/ex3_js-objects-part1/task-01.js
+++ src/ex3_js-objects-part1/task-01.js
@@ -3,9 +3,7 @@ const obj = {};
 obj.numberProp = 123;
 obj.strProp = 'str';
 obj.andOneMoreStrProp = 'str2';
-obj.showNumberProp = function showNumberProp() {
-  return this.numberProp;
-};
+obj.showNumberProp = () => this.numberProp;
 
 delete obj.andOneMoreStrProp;
 
