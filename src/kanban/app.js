6b9dec86bd34a64a360fc1beb95d15cbf50ffff7
treger78 <ilya.pustograev.i@mail.ru>
2021-10-24 15:52:09 +0300

    feat: make createNewList function, refactor dropdownUserMenu

---
+++ src/kanban/app.js
@@ -0,0 +1,14 @@
+/*
+const lists = [
+  {
+    title: 'backlog', // заголовок блока
+    tasks: [ // массив задач
+      {
+        id: 'task1',
+        name: 'Sprint bugfix',
+      },
+    ],
+  },
+  // code
+];
+*/
---
+++ src/kanban/public/scripts/createNewList.js
@@ -0,0 +1,60 @@
+const createNewListItem = document.getElementById('createNewListItem');
+
+createNewListItem.addEventListener('click', () => {
+  const createNewListButton = document.getElementById('createNewListButton');
+
+  createNewListItem.insertAdjacentHTML(
+    'afterend',
+    `
+      <form id="createNewList">
+        <label>
+          Enter list name:
+          <input id="listName" autocomplete="off" />
+        </label>
+        <div id="createNewListActionButtons">
+          <button type="button" id="createList">Create</button>
+          <button type="button" id="cancelCreate">Cancel</button>
+        </div>
+      </form>
+    `,
+  );
+
+  createNewListButton.remove();
+
+  const create = document.getElementById('createList');
+  const cancel = document.getElementById('cancelCreate');
+
+  create.addEventListener('click', () => {
+    const listName = document.getElementById('listName').value;
+    const main = document.getElementById('main');
+
+    main.insertAdjacentHTML(
+      'beforeend',
+      `
+        <div class="list">
+          <div class="listHeader">
+            <div class="listName">${listName}</div>
+            <div class="listMenu">...</div>
+          </div>
+          <div class="listTasks"></div>
+          <div class="addCard">
+            <button>+ Add card</button>
+          </div>
+        </div>
+      `,
+    );
+  });
+
+  cancel.addEventListener('click', () => {
+    document.getElementById('createNewList').remove();
+
+    createNewListItem.insertAdjacentHTML(
+      'afterbegin',
+      `
+        <button id="createNewListButton">
+          <img src="public/assets/images/add.png" />Create new list
+        </button>
+      `,
+    );
+  });
+});
