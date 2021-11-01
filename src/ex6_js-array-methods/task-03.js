7599cfd90e62b24afd406844ba6defa7019fb12a
treger78 <ilya.pustograev.i@mail.ru>
2021-11-01 21:13:36 +0300

    fix: fix trainer notes

--- src/ex6_js-array-methods/task-03.js
+++ src/ex6_js-array-methods/task-03.js
@@ -2,7 +2,7 @@ function everyPolyfill(array, callback) {
   if (array === undefined || array.length === 0) return true;
 
   for (let i = 0; i < array.length; i += 1) {
-    if (callback(array[i], i, array) === false) return false;
+    if (!callback(array[i], i, array)) return false;
   }
 
   return true;
