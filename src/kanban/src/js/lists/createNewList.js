7b35db31d2256a8dffa1e1978ed8d6cf58690aa9
treger78 <ilya.pustograev.i@mail.ru>
2021-11-11 19:59:06 +0300

    feat: make some fixes, write createNewList functions

---
+++ src/kanban/src/js/lists/createNewList.js
@@ -0,0 +1,77 @@
+/* eslint-disable import/extensions */
+// const fillLocalStorage = await import('./fillLocalStorage.js');
+// const importedLists = await import('./listsArray.js');
+// let lists = JSON.parse(localStorage.getItem(importedLists.listsArray));
+
+const createNewListButton = document.getElementById('createNewListButton');
+
+createNewListButton.addEventListener('click', () => {
+  const main = document.getElementById('main');
+
+  main.insertAdjacentHTML(
+    'afterbegin',
+    `
+    <div class="listWrapper">
+      <div class="list">
+        <div class="listHeader">
+          <input type="text" class="listName"></input>
+          <div class="listMenu">•••</div>
+        </div>
+      <div>
+    </div>
+    `,
+  );
+
+  const inputNewListName = document.getElementsByClassName('listName')[0];
+
+  inputNewListName.style.margin = '0.5rem';
+  inputNewListName.focus();
+
+  // eslint-disable-next-line consistent-return
+  inputNewListName.addEventListener('blur', () => {
+    const newListNameValue = inputNewListName.value.trim();
+
+    if (newListNameValue.replace(/\s+/g, '') === '' || newListNameValue === undefined) {
+      // eslint-disable-next-line no-alert
+      alert('Enter list name!');
+
+      document.getElementsByClassName('listWrapper')[0].remove();
+
+      return null;
+    }
+
+    const newListName = document.createElement('div');
+
+    newListName.className = 'listName';
+    newListName.innerText = newListNameValue;
+
+    inputNewListName.replaceWith(newListName);
+
+    const newListHeader = document.getElementsByClassName('listHeader')[0];
+
+    newListHeader.insertAdjacentHTML(
+      'afterend',
+      `
+        <ul class="listTasks"></ul>
+        <div class="addCard">
+          <button class="addCardButton"><img src="src/assets/images/add-card.png" /> Add card</button>
+        </div>
+      `,
+    );
+
+    /*
+    lists.unshift({
+      title: newListNameValue,
+      tasks: [],
+    });
+    */
+
+  //  fillLocalStorage(lists);
+  });
+
+  inputNewListName.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+      inputNewListName.blur();
+    }
+  });
+});
