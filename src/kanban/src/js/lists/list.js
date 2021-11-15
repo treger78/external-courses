37ce09756b2c0c78f46992f575236b9ac598a909
treger78 <ilya.pustograev.i@mail.ru>
2021-11-15 20:33:40 +0300

    refactor: change some files

--- src/kanban/src/js/lists/list.js
+++ src/kanban/src/js/lists/list.js
@@ -2,11 +2,11 @@
 let lists = await import('./listsArray.js');
 const fillLocalStorageObj = await import('../utils/lists/fillLocalStorage.js');
 const changeAddCardBtnStateObj = await import('../utils/lists/changeAddCardBtnState.js');
-const addListenersToListsObj = await import('../utils/lists/addListenersToLists.js');
+const createAndAppendDropdownTasksObj = await import('../utils/lists/createAndAppendDropdownTasks.js');
 
 const fillLocalStorage = fillLocalStorageObj.fillLocalStorage;
 const changeAddCardBtnState = changeAddCardBtnStateObj.changeAddCardBtnState;
-const addListenersToLists = addListenersToListsObj.addListenersToLists;
+const createAndAppendDropdownTasks = createAndAppendDropdownTasksObj.createAndAppendDropdownTasks;
 
 lists = lists.listsArray;
 
@@ -55,4 +55,176 @@ function insertListsFromLocalStorage(objName) {
 lists = insertListsFromLocalStorage('lists');
 lists = changeAddCardBtnState('lists');
 
+function addTaskToListByNameAndRemoveFromPreviousList(_newTaskNameValue, listIndexParam, _lists,
+  _previousListTasks, previousListIndexParam) {
+  const newTaskName = _newTaskNameValue.value;
+  const newTaskID = `${_lists[listIndexParam].tasks.length}`;
+
+  _lists[listIndexParam].tasks.push({ id: newTaskID, name: newTaskName });
+
+  const newTask = document.createElement('li');
+
+  newTask.className = 'task';
+  newTask.innerText = newTaskName;
+
+  _newTaskNameValue.replaceWith(newTask);
+
+  console.log(newTaskName);
+  for (let i = 0; i < _previousListTasks.length + 1; i += 1) {
+    console.log(_lists[previousListIndexParam].tasks[i].name);
+    if (_lists[previousListIndexParam].tasks[i].name === newTaskName) {
+      _lists[previousListIndexParam].tasks.splice(i, 1);
+
+      _previousListTasks[i].remove();
+
+      fillLocalStorage('lists', _lists);
+
+      return changeAddCardBtnState('lists');
+    }
+  }
+
+  return _lists;
+}
+
+function addListenersToLists(_lists) {
+  const listsBlocks = document.getElementById('main').children;
+
+  listsBlocks[0].getElementsByClassName('addCardButton')[0].addEventListener('click', () => {
+    const listTasks = document.getElementsByClassName('listTasks')[0];
+    const inputTaskName = document.createElement('input');
+
+    inputTaskName.className = 'task';
+    inputTaskName.style.border = 'none';
+
+    listTasks.append(inputTaskName);
+
+    inputTaskName.focus();
+
+    // eslint-disable-next-line consistent-return
+    inputTaskName.addEventListener('blur', () => {
+      const inputTaskNameValue = inputTaskName.value.trim();
+
+      if (inputTaskNameValue.replace(/\s+/g, '') === '' || inputTaskNameValue === undefined) {
+        // eslint-disable-next-line no-alert
+        alert('Enter task name!');
+
+        inputTaskName.remove();
+
+        return null;
+      }
+
+      const newTaskID = `${lists[0].tasks.length}`;
+
+      lists[0].tasks.push({ id: newTaskID, name: inputTaskNameValue });
+
+      fillLocalStorage('lists', lists);
+
+      const newTask = document.createElement('li');
+
+      newTask.className = 'task';
+      newTask.innerText = inputTaskNameValue;
+
+      inputTaskName.replaceWith(newTask);
+
+      lists = changeAddCardBtnState('lists');
+    });
+
+    inputTaskName.addEventListener('keydown', (e) => {
+      if (e.key === 'Enter') {
+        inputTaskName.blur();
+      }
+    });
+  });
+
+  for (let i = 1; i < listsBlocks.length; i += 1) {
+    listsBlocks[i].getElementsByClassName('addCardButton')[0].addEventListener('click', () => {
+      const obj = createAndAppendDropdownTasks(i, i - 1);
+      const dropdownTasks = obj.dropdownTasks;
+      const previousListTasks = obj.previousListTasks;
+
+      dropdownTasks.addEventListener('change', () => {
+        addTaskToListByNameAndRemoveFromPreviousList(dropdownTasks, i, _lists,
+          previousListTasks, i - 1);
+      });
+    });
+  }
+}
+
 addListenersToLists(lists);
+
+const createNewListButton = document.getElementById('createNewListButton');
+
+createNewListButton.addEventListener('click', () => {
+  const main = document.getElementById('main');
+
+  main.insertAdjacentHTML(
+    'afterbegin',
+    `
+    <div class="listWrapper">
+      <div class="list">
+        <div class="listHeader">
+          <input type="text" class="listName"></input>
+          <div class="listMenu">•••</div>
+        </div>
+      </div>
+    </div>
+    `,
+  );
+
+  const inputNewListName = document.getElementsByClassName('listName')[0];
+
+  inputNewListName.style.margin = '0.5rem';
+  inputNewListName.focus();
+
+  // eslint-disable-next-line consistent-return
+  inputNewListName.addEventListener('blur', () => {
+    const newListNameValue = inputNewListName.value.trim();
+
+    if (newListNameValue.replace(/\s+/g, '') === '' || newListNameValue === undefined) {
+      // eslint-disable-next-line no-alert
+      alert('Enter list name!');
+
+      document.getElementsByClassName('listWrapper')[0].remove();
+
+      return null;
+    }
+
+    const newListName = document.createElement('div');
+
+    newListName.className = 'listName';
+    newListName.innerText = newListNameValue;
+
+    inputNewListName.replaceWith(newListName);
+
+    const newListHeader = document.getElementsByClassName('listHeader')[0];
+
+    newListHeader.insertAdjacentHTML(
+      'afterend',
+      `
+        <ul class="listTasks"></ul>
+        <div class="addCard">
+          <button class="addCardButton"><img src="src/assets/images/add-card.png" /> Add card</button>
+        </div>
+      `,
+    );
+
+    lists.unshift({
+      title: newListNameValue,
+      tasks: [],
+    });
+
+    fillLocalStorage('lists', lists);
+
+    lists = changeAddCardBtnState('lists');
+
+    addListenersToLists(lists);
+
+    // location.reload();
+  });
+
+  inputNewListName.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+      inputNewListName.blur();
+    }
+  });
+});
--- src/kanban/src/js/main.js
+++ src/kanban/src/js/main.js
@@ -4,5 +4,4 @@ const header = await import('./header/header.js');
 const createNewListButton = await import('./header/createNewListButton.js');
 const dropdownUserMenu = await import('./header/dropdownUserMenu.js');
 const list = await import('./lists/list.js');
-const createNewList = await import('./lists/createNewList.js');
 const footer = await import('./footer/footer.js');
